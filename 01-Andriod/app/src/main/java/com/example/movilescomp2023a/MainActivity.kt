721cbc8f97b9fbeda984bfe546b5fb884e5ec694
Sthe12 <sthevven.morales@epn.edu.ec>
2023-06-27 10:12:48 -0500

    Union de mas codigo

--- 01-Andriod/app/src/main/java/com/example/movilescomp2023a/MainActivity.kt
+++ 01-Andriod/app/src/main/java/com/example/movilescomp2023a/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.movilescomp2023a
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+     val callbackContenidoIntentExplicito=
+         registerForActivityResult(
+             ActivityResultContracts.StartActivityForResult()
+         ){
+             result ->
+             if(result.resultCode == RESULT_OK){
+                 if(result.data != null){
+                     val data = result.data
+                     "${data?.getStringExtra("nombreModificado")}"
+                 }
+             }
+         }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
