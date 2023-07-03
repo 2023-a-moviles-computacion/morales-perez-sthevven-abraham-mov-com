cf99c362c547312ddd7dc1f90d05f226ce876c45
Sthe12 <sthevven.morales@epn.edu.ec>
2023-07-03 11:51:11 -0500

    Actualizacion

--- 01-Andriod/app/src/main/java/com/example/movilescomp2023a/ESqliteHelperEntrenador.kt
+++ 01-Andriod/app/src/main/java/com/example/movilescomp2023a/ESqliteHelperEntrenador.kt
@@ -44,3 +44,71 @@ fun crearEntrenador(
     basedatosEscritura.close()
     return if(resultadoGuardar.toInt()==-1) false else true
 }
+
+fun eliminarEntrenadorformulario(id:Int):Boolean{
+    val conexionEscritura =writableDatabase
+    val parametrosConsultaDelete = arrayOf(id.toString())
+    val resultadoEliminacion= conexionEscritura
+        .delete(
+            "ENTRENADOR",
+            "id=?",
+            parametrosConsultaDelete
+        )
+    conexionEscritura.close()
+    return if(resultadoEliminacion.toInt()== -1) false else true
+}
+
+fun actualizarEntrenadorformulario(
+    nombre: String,
+    descripcion: String,
+    id: Int,
+):Boolean{
+    val conexionEscritura = writableDatabase
+    val valoresAActualizar=ContentValues()
+    valoresAActualizar.put("nombre",nombre)
+    valoresAActualizar.put("descricion",descripcion)
+    //where id??
+    val parametrosConsultaActualizar = arrayOf(id.toString())
+    val resultadoActualizacion =conexionEscritura
+        .update(
+            "ENTRENADOR",
+            valoresAActualizar,
+            "id=?",
+            parametrosConsultaActualizar
+        )
+    conexionEscritura.close()
+    return if(resultadoActualizacion.toInt()==-1) false else true
+}
+
+fun consultarEntrenadorPorID(id: Int): BEntrenador{
+    val baseDatosLectura =readableDatabase
+    val scriptConsultaLectura = """
+        SELECT * FROM ENTRENADOR WHERE ID=?
+    """.trimIndent()
+    val parametrosConsultaLectura = arrayOf(id.toString())
+    val resultadoConsultaLectura = baseDatosLectura.rawQuery(
+        scriptConsultaLectura,
+        parametrosConsultaLectura,
+    )
+
+    //logica busqueda
+    val existeUsuario = resultadoConsultaLectura.moveToFirst()
+    val usuarioEncontrado =BEntrenador(0,"","")
+    val arreglo = arrayListOf<BEntrenador>()
+    if(existeUsuario){
+      do{
+          val id = resultadoConsultaLectura.getInt(0)
+          val nombre = resultadoConsultaLectura.getString(1)
+          val descripcion=resultadoConsultaLectura.getString(2)
+          if(id !=null){
+              //llenar el arreglo con un nuevo BEntrenador
+              usuarioEncontrado.id=id
+              usuarioEncontrado.nombre=nombre
+              usuarioEncontrado.descripcion=descripcion
+          }
+      }while(resultadoConsultaLectura.moveToNext())
+    }
+    resultadoConsultaLectura.close()
+    baseDatosLectura.close()
+    return usuarioEncontrado
+}
